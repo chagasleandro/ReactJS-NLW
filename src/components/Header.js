016e5251534ab628684e96d15f43346eb59be73a
chagasleandro <leandro_srs20@yahoo.com.br>
2023-01-24 23:31:36 -0300

    add folder src/components/Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <img 
+                src="https://github.com/chagasleandro.png" 
+                alt="Foto de Leandro Chagas perfil" />
+            </div>
+        </header>
+    )
+}
